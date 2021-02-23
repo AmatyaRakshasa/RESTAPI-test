86f43feb5f3f615c41548d860bcc705c50663359
AmatyaRakshasa <63494471+AmatyaRakshasa@users.noreply.github.com>
2021-02-23 14:52:59 -0500

    added PostgreSQL and psychopg2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from flask_restful import Api
 from db import db
@@ -10,7 +11,7 @@ from Resources.item import Item, ItemList
 from Resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'anshu'
 api = Api(app)
